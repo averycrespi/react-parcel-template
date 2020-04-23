0f6bedd1e82b04e3386ef64c401515799ab8279e
Avery Crespi <averycrespi@gmail.com>
2020-04-22 20:51:09 -0700

    Restructure src

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
+import App from "./components/App";
 import React from "react";
 import ReactDOM from "react-dom";
-
-const App = () => {
-  return <div>React Parcel Template</div>;
-};
 
 var app = document.getElementById("app");
 ReactDOM.render(<App />, app);
